565132ed4277224579ca4298c41c6e1d618cbd03
Kisora Thomas <kisora.thomas@gmail.com>
2024-11-01 23:38:50 -0600

    Update tier pricing heights and formatting
    Standardized pricing text to uppercase and adjusted component heights for tiers in the Pricing view. These changes ensure consistent visual alignment and readability of subscription plans.

--- src/views/Pricing.vue
+++ src/views/Pricing.vue
@@ -6,18 +6,19 @@ import Tier from "@/components/Tier.vue";
 
 <template>
   <div class="wrap">
+
     <div class="titleWrap">
       <div class="title">PRICING</div>
       <div class="subtitle">{{data.pricing.intro}}</div>
     </div>
+
     <div class="priceWrap">
-
-      <Tier title=" TIER 00 / " price=" $49 / mo " :data="data.pricing.tiers.tier0"/>
-      <Tier title=" TIER 01 / " price=" $99 / mo " :data="data.pricing.tiers.tier1"/>
-      <Tier title=" TIER 02 / " price=" $149 / mo " :data="data.pricing.tiers.tier2"/>
-      <Tier title=" TIER 03 / " price=" $199 / mo " :data="data.pricing.tiers.tier3"/>
-
+      <Tier title=" TIER 00 / " price=" $49 / MO " :data="data.pricing.tiers.tier0" style="height: 80%"/>
+      <Tier title=" TIER 01 / " price=" $99 / MO " :data="data.pricing.tiers.tier1" style="height: 100%"/>
+      <Tier title=" TIER 02 / " price=" $149 / MO " :data="data.pricing.tiers.tier2" style="height: 100%"/>
+      <Tier title=" TIER 03 / " price=" $199 / MO " :data="data.pricing.tiers.tier3" style="height: 80%"/>
     </div>
+
   </div>
 </template>
 
